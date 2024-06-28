8ca0d71770897a0abedf2b831a084c58e361a281
RJW20 <robertjwatts84@outlook.com>
2024-06-28 20:50:54 +0100

    Implement the RewardTable and some basics for the QTable

--- q_learning/q_table.py
+++ q_learning/q_table.py
@@ -1,5 +1,34 @@
-
+from q_learning.state import State, StateError
+from q_learning.action import Action
 
 
 class QTable:
-    """Q-table developed by the Agent to choose Actions."""
+    """Q-table developed by the Agent to choose Actions.
+    
+    Implemented as a dictionary with States as the keys and a list of tuples
+    of Actions and their respective Q-value as values.
+    """
+
+    def __init__(self, states_and_actions: list[State, list[Action]] | None = None) -> None:
+
+        self.table = dict()
+        if states_and_actions:
+            for state, actions in states_and_actions:
+                self.add_actions(state, actions)
+
+    def add_actions(self, state: State, actions: tuple[Action]) -> None:
+        """Create an entry self.table[state] that is a list of tuples of the given 
+        actions and the initial value 0.
+        
+        If self.table[state] already exists throws a StateError.
+        """
+
+        if state in self.table:
+            raise StateError(f'The State {state} already exists in the QTable, please check your ' + \
+                             'list of States for duplicates.')
+        
+        self.table[state] = [(action, 0) for action in actions]
+
+    def available_actions(self, state: State) -> list[tuple[Action, float]]:
+        """Return the list of available Actions and their Q-values at the given State."""
+        return self.table[state]
--- q_learning/reward_table.py
+++ q_learning/reward_table.py
@@ -1,6 +1,40 @@
-
+from q_learning.state import State, StateError
+from q_learning.action import Action
 
 
 class RewardTable:
     """Table of values indicating the reward the Agent receives by making a given
-    Action at a given State."""
+    Action at a given State.
+    
+    Implemented as dictionary with tuples of States and Actions as the keys and the
+    reward as the value.
+    """
+
+    def __init__(self, states_actions_rewards: list[tuple[State, Action, float]] | None = None) -> None:
+
+        self.table = dict()
+        if states_actions_rewards:
+            for state, action, reward in states_actions_rewards:
+                self.add_reward(state, action, reward)
+
+    def add_reward(self, state: State, action: Action, reward: float) -> None:
+        """Create an entry in self.table with key (state, action) and value reward.
+        
+        If self.table[(state, action)] already exists throws a StateError.
+        """
+
+        if (state, action) in self.table:
+            raise StateError(f'The State-Action pair {(state, action)} already exists in the RewardTable, please check your ' + \
+                             'list of States and Actions for duplicates.')
+        
+    def reward_for(self, action: Action, state: State) -> float:
+        """Return the reward for taking the given Action at the given State.
+        
+        If self.table[(state, action)] does not exist throws a StateError.
+        """
+
+        try:
+            return self.table[(state, action)]
+        except KeyError:
+            raise StateError(f'The State-Action pair {(state, action)} does not exist in the RewardTable, please check your ' + \
+                             'list of States and Actions is comprehensive.')
