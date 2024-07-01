e6235d74e84bbb8a53b2ebf14b5e2e2075cad4af
RJW20 <robertjwatts84@outlook.com>
2024-07-01 15:01:33 +0100

    Condense config into one settings file

---
+++ mouse_and_cheese/settings.py
@@ -0,0 +1,12 @@
+
+
+settings = {
+
+    # Layout settings
+    'grid_size': (10, 10),
+
+    # Q-learning settings
+    'learning_rate': 1,
+    'discount_factor': 0.5,
+
+}
---
+++ q_learning/settings.py
@@ -0,0 +1,8 @@
+
+
+settings = {
+
+    'learning_rate': 1,
+    'discount_factor': 0.5,
+
+}
