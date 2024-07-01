5cb61e0a02de31406ca831085047879c34ea98bc
RJW20 <robertjwatts84@outlook.com>
2024-07-01 08:35:02 +0100

    Fully implement the RewardTable class over the top of the Table class

--- q_learning/reward_table.py
+++ q_learning/reward_table.py
@@ -1,40 +1,83 @@
-from q_learning.state import State, StateError
 from q_learning.action import Action
+from q_learning.state import State, StateError
+from q_learning.table import ColumnError, RowError, Table
 
 
-class RewardTable:
-    """Table of values indicating the reward the Agent receives by making a given
-    Action at a given State.
-    
-    Implemented as dictionary with tuples of States and Actions as the keys and the
-    reward as the value.
-    """
+class RewardTable(Table):
+    """Table of values indicating the reward the Agent receives by making a
+    given Action at a given State."""
 
-    def __init__(self, states_actions_rewards: list[tuple[State, Action, float]] | None = None) -> None:
+    def __init__(
+        self,
+        states_actions_rewards: list[tuple[State, list[tuple[Action, float]]]]
+        | None = None,
+    ) -> None:
+        
+        super().__init__()
 
-        self.table = dict()
         if states_actions_rewards:
-            for state, action, reward in states_actions_rewards:
-                self.add_reward(state, action, reward)
+            for state, actions_and_rewards in states_actions_rewards:
+                self.new_rewards(state, actions_and_rewards)
 
-    def add_reward(self, state: State, action: Action, reward: float) -> None:
-        """Create an entry in self.table with key (state, action) and value reward.
-        
-        If self.table[(state, action)] already exists throws a StateError.
-        """
+    def new_rewards(
+        self,
+        state: State,
+        actions_and_rewards: list[tuple[Action, float]],
+    ) -> None:
+        """Create an entry in self for row State with the given Actions and
+        corresponding rewards.
 
-        if (state, action) in self.table:
-            raise StateError(f'The State-Action pair {(state, action)} already exists in the RewardTable, please check your ' + \
-                             'list of States and Actions for duplicates.')
-        
-    def reward_for(self, action: Action, state: State) -> float:
-        """Return the reward for taking the given Action at the given State.
-        
-        If self.table[(state, action)] does not exist throws a StateError.
+        If the row State already exists then raises a StateError.
         """
 
         try:
-            return self.table[(state, action)]
-        except KeyError:
-            raise StateError(f'The State-Action pair {(state, action)} does not exist in the RewardTable, please check your ' + \
-                             'list of States and Actions is comprehensive.')
+            super().new_row(state, actions_and_rewards)
+        except RowError:
+            raise StateError(
+                f"The State {state} already exists in the RewardTable, please "
+                + "check your list of States for duplicates."
+            )
+
+    def __setitem__(self, state: State, action: Action, reward: float) -> None:
+        """Set the reward for taking the given Action from the given State.
+        
+        If the row State doesn't exist then raises a StateError.
+        If the action Action doesn't does't exist for the State then raises an
+        ActionError.
+        """
+
+        try:
+            super.__setitem__(state, action, reward)
+        except RowError:
+            raise StateError(
+                f"The State {state} does not exist in the RewardTable, please "
+                + "check your initial list of States is comprehensive."
+            )
+        except ColumnError:
+            raise Action(
+                f"The State-Action pair {(state, action)} does not exist in "
+                + "the RewardTable, please check your initial list of States "
+                + "and Actions is comprehensive."
+            )
+        
+    def __getitem__(self, state: State, action: Action) -> float:
+        """Get the reward for taking the given Action from the given State.
+        
+        If the row State doesn't exist then raises a StateError.
+        If the action Action doesn't does't exist for the State then raises an
+        ActionError.
+        """
+
+        try:
+            return super.__getitem__(state, action)
+        except RowError:
+            raise StateError(
+                f"The State {state} does not exist in the RewardTable, please "
+                + "check your initial list of States is comprehensive."
+            )
+        except ColumnError:
+            raise Action(
+                f"The State-Action pair {(state, action)} does not exist in "
+                + "the RewardTable, please check your initial list of States "
+                + "and Actions is comprehensive."
+            )
