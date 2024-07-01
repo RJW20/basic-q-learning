47203a5b7a9d17f0bddcb608dfc255b0fb804542
RJW20 <robertjwatts84@outlook.com>
2024-07-01 08:41:42 +0100

    Implement some of the QTable class

--- q_learning/q_table.py
+++ q_learning/q_table.py
@@ -1,34 +1,35 @@
-from q_learning.state import State, StateError
 from q_learning.action import Action
+from q_learning.state import State, StateError
+from q_learning.table import RowError, Table
 
 
-class QTable:
-    """Q-table developed by the Agent to choose Actions.
-    
-    Implemented as a dictionary with States as the keys and a list of tuples
-    of Actions and their respective Q-value as values.
-    """
+class QTable(Table):
+    """Q-table developed by the Agent to choose Actions."""
 
-    def __init__(self, states_and_actions: list[State, list[Action]] | None = None) -> None:
+    def __init__(
+        self,
+        states_and_actions: list[tuple[State, list[Action]]] | None = None,
+    ) -> None:
 
-        self.table = dict()
+        super().__init__()
+
         if states_and_actions:
             for state, actions in states_and_actions:
-                self.add_actions(state, actions)
+                self.new_actions(state, actions)
 
-    def add_actions(self, state: State, actions: tuple[Action]) -> None:
-        """Create an entry self.table[state] that is a list of tuples of the given 
-        actions and the initial value 0.
+    def new_actions(self, state: State, actions: list[Action]) -> None:
+        """Create an entry in self for the row State with the given Actions and
+        the initial values 0.
         
-        If self.table[state] already exists throws a StateError.
+        If the row State already exists then raises a StateError.
         """
 
-        if state in self.table:
-            raise StateError(f'The State {state} already exists in the QTable, please check your ' + \
-                             'list of States for duplicates.')
-        
-        self.table[state] = [(action, 0) for action in actions]
+        actions_and_initials = [(action, 0) for action in actions]
 
-    def available_actions(self, state: State) -> list[tuple[Action, float]]:
-        """Return the list of available Actions and their Q-values at the given State."""
-        return self.table[state]
+        try:
+            super().new_row(state, actions_and_initials)
+        except RowError:
+            raise StateError(
+                f"The State {state} already exists in the QTable, please "
+                + "check your list of States for duplicates."
+            )
