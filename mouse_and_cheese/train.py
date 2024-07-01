3885efedffd8f916d17309de954afc9c5f76bc7e
RJW20 <robertjwatts84@outlook.com>
2024-07-01 15:14:31 +0100

    Rework Train initialisation

--- mouse_and_cheese/train.py
+++ mouse_and_cheese/train.py
@@ -20,50 +20,42 @@ class Train(BaseVisual):
         super().__init__(settings)
         pg.display.set_caption("Mouse finds Cheese")
 
+        # Store the set up
         self.cats = cats
         self.cheese = cheese
 
+        # Unpack the settings
         learning_rate = settings['learning_rate']
         discount_factor = settings['discount_factor']
 
-        # Create all the States and Actions
-        states = {State((x, y)) for x in range(0, self.grid_size[0])
-                  for y in range(0, self.grid_size[1])}
-        actions = [
-            Action(lambda x: (x[0], x[1] - 1)),     # Up
-            Action(lambda x: (x[0] + 1, x[1])),     # Right
-            Action(lambda x: (x[0], x[1] + 1)),     # Down
-            Action(lambda x: (x[0] - 1, x[1])),     # Left
-        ]
-
-        # Pair all valid States and Actions
+        # Create the QTable
         states_and_actions = [
             (state, [
-                action for action in actions
+                action for action in self.actions
                 if state._identifier not in self.cats | {self.cheese} and
-                action.act_on(state) in states
+                action.act_on(state) in self.states
             ])
-            for state in states
+            for state in self.states
         ]
+        q_table = QTable(states_and_actions)
 
-        # Set up all rewards
+        # Create the RewardTable
         states_actions_rewards = []
         for state, actions in states_and_actions:
             actions_rewards = []
             for action in actions:
                 if (next_identifier := action.act_on(state)._identifier) \
                     in self.cats:
-                    reward = -10
+                    reward = -10    # -10 for hitting a Cat
                 elif next_identifier == self.cheese:
-                    reward = 100
+                    reward = 100    # 100 for eating the Cheese
                 else:
                     reward = 0
                 actions_rewards.append((action, reward))
             states_actions_rewards.append((state, actions_rewards))
-
-        # Load all components
         reward_table = RewardTable(states_actions_rewards)
-        q_table = QTable(states_and_actions)
+        
+        # Create the Agent
         self.mouse = Agent(
             State((start)),
             reward_table,
@@ -71,6 +63,31 @@ class Train(BaseVisual):
             learning_rate,
             discount_factor,
         )
+
+    @property
+    def states(self) -> list[State]:
+        """Return a list of all possible States.
+        
+        This is comprised of all positions contained in the grid with size
+        self.grid_size.
+        """
+
+        return {State((x, y)) for x in range(0, self.grid_size[0])
+                  for y in range(0, self.grid_size[1])}
+    
+    @property
+    def actions(self) -> list[Action]:
+        """Return a list of all possible Actions.
+        
+        This is comprised of movements to adjacent grid positions.
+        """
+
+        return [
+            Action(lambda x: (x[0], x[1] - 1)),     # Up
+            Action(lambda x: (x[0] + 1, x[1])),     # Right
+            Action(lambda x: (x[0], x[1] + 1)),     # Down
+            Action(lambda x: (x[0] - 1, x[1])),     # Left
+        ]
 
     def check_events(self) -> None:
         """Check for new user inputs."""
