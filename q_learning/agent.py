9363c387a6eadc93cfd20f2af9421e5450e890ad
RJW20 <robertjwatts84@outlook.com>
2024-07-01 10:06:24 +0100

    Implement transitioning between States for the Agent

--- q_learning/agent.py
+++ q_learning/agent.py
@@ -1,6 +1,7 @@
-from q_learning.state import State
-from q_learning.reward_table import RewardTable
+from q_learning.action import Action
 from q_learning.q_table import QTable
+from q_learning.reward_table import RewardTable
+from q_learning.state import State
 
 
 class Agent:
@@ -11,15 +12,40 @@ class Agent:
         starting_state: State,
         reward_table: RewardTable,
         q_table: QTable,
+        learning_rate: float,
+        discount_rate: float,
     ) -> None:
 
-        self.state: State = starting_state
+        self.current_state: State = starting_state
+        self.previous_state: State | None = None
+        self.previous_action: Action
+        self.previous_reward: float
+
         self.reward_table: RewardTable = reward_table
         self.q_table: QTable = q_table
+
+        self.learning_rate: float = learning_rate
+        self.discount_rate: float = discount_rate
 
     def next_state(self) -> None:
         """Choose and perform an Action using self.q_table.
         
-        Updates self.q_table based on the reward received for taking the Action 
-        as found in self.reward_table.
+        Updates the Q-value for the previous state before advancing.
         """
+
+        action, q = self.q_table.best_action(self.current_state)
+
+        if self.previous_state:
+            self.q_table.update(
+                self.previous_state,
+                self.previous_action,
+                self.previous_reward,
+                self.learning_rate,
+                self.discount_rate,
+                q,
+            )
+
+        self.previous_state = self.current_state
+        self.previous_action = action
+        self.previous_reward = self.reward_table[self.current_state, action]
+        self.current_state = action.act_on(self.current_state)
